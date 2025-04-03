9a9a1c317bc8ce19f0568cc36bb061ad7ca5acae
LeeH <202130126@email.daelim.ac.kr>
2025-04-03 12:23:35 +0900

    v0.8 week05

---
+++ week05_prime.py
@@ -0,0 +1,29 @@
+def is_prime(number : int) -> bool:
+    """
+
+    :param number:
+    :return: True if it is prime number else False
+    """
+    is_prime = True
+
+    if number >= 2:
+       for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                return False
+            #print(i, end=" ")
+
+    else:
+        return False
+    return True
+
+n1 = int(input())
+n2 = int(input())
+
+for i in range(n1, n2+1):
+    if is_prime(i):
+        print(i, end=' ')
+# number = int(input())
+# if is_prime:
+#     print(f"{number}는 소수입니다")
+# else:
+#     print(f"{number}는 소수가 아닙니다")
