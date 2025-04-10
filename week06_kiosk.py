905ad6a0a053146ac0dde4e3a0b43646b8020274
LeeH <202130126@email.daelim.ac.kr>
2025-04-10 11:28:40 +0900

    v1.1 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -3,29 +3,33 @@ drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스"]
 prices = [1500, 2500, 4000]
 total_price = 0
 amounts = [0, 0, 0]
-#order_list =""
 menu_texts = ""
+
+def order_process(idx):
+    """
+    주문 처리 함수 1) 주문 디스플레이 2)총 주문 금액 누산 3) 수량 업데이트  
+    :return:없음
+    """
+    global total_price #global- 전역변수
+    print(f"{drinks[idx]}를 주문하셨습니다. 가격은 {prices[idx]}원 입니다")
+    total_price = total_price + prices[idx]
+    amounts[idx] = amounts[idx] + 1
+
+
 for j in range(len(drinks)):
     menu_texts = menu_texts + f"{j+1}) {drinks[j]} {prices[j]}원"
 menu_texts = menu_texts + f"{len(drinks)+1} 주문종료 : "
-while True:                              #배열 순서
 
+while True:
     menu = input(menu_texts)
     if menu =="1":   #f는 f-string으로 문자열 안에서 변수, 함수 사용가능
-
-        print(f"{drinks[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
-        total_price = total_price + prices[0]
-        amounts[0] = amounts[0] + 1
+        order_process(int(menu) -1)
 
     elif menu =="2":
-        print(f"{drinks[1]}를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
-        total_price = total_price + prices[1]
-        amounts[1] = amounts[1] + 1
+        order_process(int(menu) - 1)
 
     elif menu == "3":
-        print(f"{drinks[2]}를 주문하셨습니다. 가격은 {prices[2]}원 입니다")
-        total_price = total_price + prices[2]
-        amounts[2] = amounts[2] + 1
+        order_process(int(menu) -1)
 
     elif menu == "4":
         print("주문을 종료합니다")
@@ -33,10 +37,6 @@ while True:                              #배열 순서
     else:
         print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
 
-#print(order_list)
-# print(f"{drinks[0]} {prices[0]}원 {amounts[0]}잔 {prices[0] * amounts[0]}원")
-# print(f"{drinks[1]} {prices[1]}원 {amounts[1]}잔 {prices[1] * amounts[1]}원")
-# print(f"{drinks[2]} {prices[2]}원 {amounts[2]}잔 {prices[2] * amounts[2]}원")
 print("상품명 단가 수량 금액")
 for i in range(len(drinks)):
     if amounts[i] > 0:
