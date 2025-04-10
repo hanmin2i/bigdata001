f1c0ffbfb6d488cb889fcd04b8926b2189a892c5
LeeH <202130126@email.daelim.ac.kr>
2025-04-10 10:40:49 +0900

    v0.6 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -2,14 +2,17 @@
 drinks = ["아이스 아메리카노", "카페 라떼"]
 prices = [1500, 2500]
 total_price = 0
+order_list =""
 while True:                              #배열 순서
     menu = input(f"1) {drinks[0]} {prices[0]}원 2) {drinks[1]} {prices[1]}원 3) 주문종료 : ")
     if menu =="1":   #f는 f-string으로 문자열 안에서 변수, 함수 사용가능
         print(f"{drinks[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
         total_price = total_price + prices[0]
+        order_list = order_list + drinks[0] + '\n'
     elif menu =="2":
         print(f"{drinks[1]}를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
         total_price = total_price + prices[1]
+        order_list = order_list + drinks[1] + '\n'
     elif menu == "3":
         print("주문을 종료합니다")
         break
