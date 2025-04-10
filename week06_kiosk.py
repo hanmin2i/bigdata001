5bd8ca639b896778b31ec0201be7be324f7d5021
LeeH <202130126@email.daelim.ac.kr>
2025-04-10 11:21:33 +0900

    v1.0 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -9,24 +9,24 @@ for j in range(len(drinks)):
     menu_texts = menu_texts + f"{j+1}) {drinks[j]} {prices[j]}원"
 menu_texts = menu_texts + f"{len(drinks)+1} 주문종료 : "
 while True:                              #배열 순서
-   # menu = input(f"1) {drinks[0]} {prices[0]}원 2) {drinks[1]} {prices[1]}원 3) {drinks[2]} {prices[2]}원 4) 주문 종료 : ")
+
     menu = input(menu_texts)
     if menu =="1":   #f는 f-string으로 문자열 안에서 변수, 함수 사용가능
 
         print(f"{drinks[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다")
         total_price = total_price + prices[0]
         amounts[0] = amounts[0] + 1
-       # order_list = order_list + drinks[0] + '\n'
+
     elif menu =="2":
         print(f"{drinks[1]}를 주문하셨습니다. 가격은 {prices[1]}원 입니다")
         total_price = total_price + prices[1]
         amounts[1] = amounts[1] + 1
-        #order_list = order_list + drinks[1] + '\n'
+
     elif menu == "3":
         print(f"{drinks[2]}를 주문하셨습니다. 가격은 {prices[2]}원 입니다")
         total_price = total_price + prices[2]
         amounts[2] = amounts[2] + 1
-        # order_list = order_list + drinks[1] + '\n'
+
     elif menu == "4":
         print("주문을 종료합니다")
         break
@@ -39,5 +39,6 @@ while True:                              #배열 순서
 # print(f"{drinks[2]} {prices[2]}원 {amounts[2]}잔 {prices[2] * amounts[2]}원")
 print("상품명 단가 수량 금액")
 for i in range(len(drinks)):
-    print(f"{drinks[i]} {prices[i]} {amounts[i]} {prices[i] * amounts[i]}")
-print(f"총 주문 금액 : {total_price}")
+    if amounts[i] > 0:
+        print(f"{drinks[i]} {prices[i]} {amounts[i]} {prices[i] * amounts[i]}")
+print(f"총 주문 금액 : {total_price}원")
