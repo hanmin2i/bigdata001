fd1bd64f9833c0bb3959ec6581f4214728e708cd
LeeH <202130126@email.daelim.ac.kr>
2025-04-10 11:48:20 +0900

    v1.4 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,9 +1,10 @@
 drinks = ["아이스 아메리카노", "카페라떼", "수박주스" , "딸기주스"]    #len은 리스트 안에 들어있는 원소 갯수
 prices = [1500, 2500, 4000, 4500]
 total_price = 0
-amounts = list()
-for _ in range(len(drinks)):
-    amounts.append(0)      #append 리스트안에 데이터 추가
+# amounts = list()
+# for _ in range(len(drinks)):
+#     amounts.append(0)      #append 리스트안에 데이터 추가
+amounts = [0 for _ in range(len(drinks))] # list comprehension (리스트 축약) _는 변수 없이 그냥 사용할때
 menu_texts = ""
 
 def order_process(idx):
