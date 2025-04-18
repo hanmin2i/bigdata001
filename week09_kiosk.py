9fc15f417645562a9fcb647087f9299b27b91ae9
LeeH <202130126@email.daelim.ac.kr>
2025-04-18 11:06:29 +0900

    v2.3 kiosk replace

--- week09_kiosk.py
+++ week09_kiosk.py
@@ -1,20 +1,21 @@
-import kiosk
 
+import kiosk as kk
+# crtl + r
 
 # 실제론 영향을 안받음 타입 힌트 -> 리턴값
 
 while True:
     try:
 
-        menu = int(input(kiosk.display_menu()))
-        if len(kiosk.drinks) >= menu >= 1:
-            kiosk.order_process(menu - 1)
+        menu = int(input(kk.display_menu()))
+        if len(kk.drinks) >= menu >= 1:
+            kk.order_process(menu - 1)
 
-        elif menu == len(kiosk.drinks) + 1:
+        elif menu == len(kk.drinks) + 1:
             print(" 주문을 종료합니다")
             break
         else:
             print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
     except ValueError:
         print("숫자를 입력해주십쇼")
-kiosk.print_receipt()
+kk.print_receipt()
