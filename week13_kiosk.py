7463b5c9306c2b4b430d4653f318e8fb23a2e625
LeeH <202130126@email.daelim.ac.kr>
2025-05-29 10:43:13 +0900

    v3.3 kiosk api error fix

--- week13_kiosk.py
+++ week13_kiosk.py
@@ -4,9 +4,17 @@ import kiosk as kk
 import requests
 
 if __name__ == "__main__":
-    url = f"https://wttr.in/suwon?format=%C+%t&lang=ko"
-    response = requests.get(url)
-    print(response.text.strip())
+    # url = f"https://wttr.in/suwon?format=%C+%t&lang=ko"
+    #url = f"https://naver.com/kk"
+    url = f"https://wttr123.in/suwon?format=%C+%t&lang=ko"
+    try:
+        response = requests.get(url)
+        if response.status_code ==200:
+            print(response.text.strip())
+        else:
+            print(f"상태코드 : {response.status_code}")
+    except Exception as err:
+        print(f"오류 코드 : {err}")
     kk.run()
     kk.print_receipt()
     kk.print_ticket_number()
